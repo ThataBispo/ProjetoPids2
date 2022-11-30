bdeddee8c0e331c9ed14a8b10211f9916fe31708
rodrigo322 <rodrigollrdglucas@gmail.com>
2022-11-29 22:01:57 -0300

    teacherlist

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import landingImg from "../../assets/landing.png";
 import logoImg from "../../assets/logo.png";
 import studyIcon from "../../assets/images/icons/study.svg";
@@ -18,15 +20,15 @@ function Landing() {
         <img src={landingImg} alt="" className="hero-image" />
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Aluno" />
             Aluno
-          </a>
+          </Link>
 
-          <a href="" className="give-classes">
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Professor" />
             Professor
-          </a>
+          </Link>
         </div>
 
         <span className="total">
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import backIcon from "../../assets/images/icons/back.svg";
 
+import "./styles.css";
+
+import logoImg from "../../assets/logo.png";
 function TeacherList() {
   return (
-    <h1>Professores</h1>
-  )
+    <div id="page-teacher-list" className="container">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="Voltar" />
+          </Link>
+          <img src={logoImg} alt="OnTeacher" />
+        </div>
 
+        <div className="header-content">
+          <strong>Professores Disponíveis</strong>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default TeacherList;
