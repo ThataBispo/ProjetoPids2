2686a83f17f794e1ab8a2e559ed2bb4a0cc7d188
rodrigo322 <rodrigollrdglucas@gmail.com>
2022-11-29 21:08:45 -0300

    initial commit

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,8 @@
-import landingImg from "../../assets/images/landing.svg";
-import logoImg from "../../assets/images/logo.svg";
+import landingImg from "../../assets/landing.png";
+import logoImg from "../../assets/logo.png";
+import studyIcon from "../../assets/images/icons/study.svg";
+import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 
 import "./styles.css";
 
@@ -8,10 +11,27 @@ function Landing() {
     <div id="page-landing">
       <div id="page-landing-content" className="container">
         <div className="logo-container">
-          <img className="img" src={logoImg} alt="EstudeAqui" />
+          <img src={logoImg} alt="Proffy" />
           <h2>Sua plataforma de estudos online.</h2>
         </div>
+
         <img src={landingImg} alt="" className="hero-image" />
+
+        <div className="buttons-container">
+          <a href="" className="study">
+            <img src={studyIcon} alt="Aluno" />
+            Aluno
+          </a>
+
+          <a href="" className="give-classes">
+            <img src={giveClassesIcon} alt="Professor" />
+            Professor
+          </a>
+        </div>
+
+        <span className="total">
+          Estude conosco. <img src={purpleHeartIcon} alt="Coração roxo" />
+        </span>
       </div>
     </div>
   );
---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function TeacherList() {
+  return (
+    <h1>Professores</h1>
+  )
+
+}
+
+export default TeacherList;
